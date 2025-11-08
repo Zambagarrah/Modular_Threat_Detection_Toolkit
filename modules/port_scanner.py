a7ac31014dcc1a94fa4706842b0f3a3b04a6eb44
Zambagarrah <zambagarrah@users.noreply.github.com>
2025-11-08 15:57:38 +0300

    Imported socket & argparse

---
+++ modules/port_scanner.py
@@ -0,0 +1,3 @@
+import socket #Built-in networking library. It lets us connect to IPs and ports.
+import argparse #Makes our script usable from the command line with flags like --host and --ports.
+
